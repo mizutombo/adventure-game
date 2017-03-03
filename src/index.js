b4793c470b568494b007e66b7ed3b7dc62eb92b6
mericks <mericks@users.noreply.github.com>
2017-03-03 15:40:12 -0800

    project files set up

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App'; // in _forReference folder
+// import './index.css';  // in _forReference folder
+
+ReactDOM.render(
+  <h1>Hello world</h1>,
+  document.getElementById('root')
+);
