b25e81029412f5ebdebca045ab403ff9b1c486d7
Clarefollett <chlaw101@gmail.com>
2017-03-06 10:12:31 -0800

    welcome to pioneer square header is component

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 // import App from './App'; // in _forReference folder
 // import './index.css';  // in _forReference folder
 
+//turning header into component
+function Pioneer(props) {
+  return (
+    <h1>Welcome to Pioneer Square!</h1>
+  )
+}
+
 ReactDOM.render(
-  <h1>Hello world</h1>,
+  <Pioneer />,
   document.getElementById('root')
 );
